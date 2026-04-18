cba86d2425e826d1401f129bf7082cb8b50bd99d
losion <losion445@gmail.com>
2026-04-18 03:36:48 +0300

    [INFRASTRUCTURE] Frontend vite + react + tailwindcss setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Tailwind is Working!
+      </h1>
+  )
+}
+
+export default App
